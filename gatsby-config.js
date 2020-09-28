9d32f80e3f55ca2d3c501b4a8b8e369f6bbd0522
Alexandr Kolesnik <alexandr.kolesnik.92@gmail.com>
2020-09-28 16:31:40 -0700

    Full width images.

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,7 +23,8 @@ module.exports = {
           {
             resolve: 'gatsby-remark-images',
             options: {
-              maxWidth: 960
+              maxWidth: 960,
+              wrapperStyle: 'max-width: 960px; width: 100%;'
             }
           }
         ],
